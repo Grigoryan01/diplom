ecb5c4d541ea73f673ae12d160674087c1e9b652
Grigoryan <grigoryanelmira01@gmail.com>
2025-06-12 00:27:22 +0400

    Create Home page

---
+++ src/components/home-page/Homepage.tsx
@@ -0,0 +1,15 @@
+import Footer from "../../reusable/Footer.tsx"
+import Navbar from "../../reusable/Header.tsx"
+import Content from "./content.tsx"
+const HomePage=()=>{
+    return(
+        <>
+        <Navbar />
+        <Content />
+        <Footer />
+        </>
+    )
+}
+
+
+export default HomePage
---
+++ src/dashboard/home/home.tsx
@@ -0,0 +1,10 @@
+import HomePage from "../../components/home-page/Homepage.tsx"
+
+const Home=()=>{
+    return(
+        <HomePage />
+    )
+}
+
+
+export default Home;
