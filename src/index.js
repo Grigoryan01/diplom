f2536989941a8a40661b1a1ea6876610cad30763
Grigoryan <grigoryanelmira01@gmail.com>
2025-06-12 22:09:07 +0400

    Create SingIn, SignUp page

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import PrivacyPolicyPage from './dashboard/terms/privacy.tsx';
+import SignIn from './components/sign-in/signin.tsx';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PrivacyPolicyPage />
+    <SignIn />
   </React.StrictMode>
 );
 
